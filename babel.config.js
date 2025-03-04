d53c716452388bacded4ec16b3bc8c6a057f5df4
Gustavo Cortez <cmgustavo83@gmail.com>
2025-03-03 23:47:32 -0300

    fix react-native-paper for dropdown

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  /*
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
     },
+    development: {
+      plugins: ['react-native-paper/babel'],
+    },
   },
+   */
 };
