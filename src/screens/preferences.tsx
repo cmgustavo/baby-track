d26f2da3b87aef88449a5120f43505846652183a
Gustavo Cortez <cmgustavo83@gmail.com>
2025-04-23 00:58:55 -0300

    Bump 1.0.6(16)

--- src/screens/preferences.tsx
+++ src/screens/preferences.tsx
@@ -67,7 +67,7 @@ const Preferences = ({navigation}) => {
           <List.Subheader>
             <Text variant="titleMedium">About</Text>
           </List.Subheader>
-          <List.Item title="Version" right={props => <Text>v1.0.5</Text>} />
+          <List.Item title="Version" right={props => <Text>v1.0.6</Text>} />
           <Divider />
         </List.Section>
       </View>
